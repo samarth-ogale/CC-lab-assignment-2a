d3ef1152a215c651b5a4dbcd99b67c001a32bf41
samarth-ogale <omogale121@gmail.com>
2023-02-16 19:33:19 +0530

    pipeline

---
+++ main/task5.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, CC Lab\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES2UG21CS814 created a job in jenkins\n";
+  return 0;
+}
